f522451a709b56a585b962b010fa9b28f2b534b0
Mark Anderson <leNkusWoaceN.github.com@ma7.org>
2015-01-06 20:40:48 -0600

    two day we want

--- get_data.R
+++ get_data.R
@@ -14,13 +14,35 @@ setwd('~/leNkusWoaceN/ExData_Plotting1')
 
 url  <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 file <- 'household_power_consumption.zip'
+uncompress.file <- 'household_power_consumption.txt'
 
 # get file is doesn't already exist
-if (!file.exists(file)) {
-    download.file(url,file,method='curl')
-    dateDownloaded <- date()
-    dateDownloaded
+if (!file.exists(uncompress.file)) {
+    if (!file.exists(file)) {
+        download.file(url,file,method='curl')
+        dateDownloaded <- date()
+        dateDownloaded
+    }
+    # unzip .zip file
+    unzip(file)
 }
 
-# unzip .zip file
-unzip(file)
+fileColClasses <- function(filename) {
+  pre         <- read.table(filename,header=TRUE,nrows=1)
+  classes     <- sapply(pre, class)
+}
+
+household.power.consumption <- read.table(
+  uncompress.file,
+  header=TRUE,
+  na.strings='?',
+  sep=';',
+  colClasses=fileColClasses(uncompress.file)
+)
+
+power.consumption <- household.power.consumption[
+  household.power.consumption$Date=='1/2/2007' | 
+  household.power.consumption$Date=='2/2/2007',
+]
+
+head(power.consumption)

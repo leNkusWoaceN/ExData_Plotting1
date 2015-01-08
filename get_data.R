10b50223a479ad761011b2421e577c7b4542748d
Mark Anderson <leNkusWoaceN.github.com@ma7.org>
2015-01-07 23:21:55 -0600

    DateTime and functionize

--- get_data.R
+++ get_data.R
@@ -10,39 +10,66 @@
 # one-minute sampling rate over a period of almost 4 years. Different electrical 
 # quantities and some sub-metering values are available.
 
-setwd('~/leNkusWoaceN/ExData_Plotting1')
-
-url  <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
-file <- 'household_power_consumption.zip'
-uncompress.file <- 'household_power_consumption.txt'
-
-# get file is doesn't already exist
-if (!file.exists(uncompress.file)) {
-    if (!file.exists(file)) {
-        download.file(url,file,method='curl')
-        dateDownloaded <- date()
-        dateDownloaded
-    }
-    # unzip .zip file
-    unzip(file)
-}
+library("lubridate")
 
 fileColClasses <- function(filename) {
-  pre         <- read.table(filename,header=TRUE,nrows=1)
-  classes     <- sapply(pre, class)
+  pre  <- read.table(
+      filename,
+      header=TRUE,
+      nrows=1,
+      na.strings='?',
+      sep=';'
+    )
+  classes  <- sapply(pre, class)
 }
 
-household.power.consumption <- read.table(
-  uncompress.file,
-  header=TRUE,
-  na.strings='?',
-  sep=';',
-  colClasses=fileColClasses(uncompress.file)
-)
+getData <- function() {
 
-power.consumption <- household.power.consumption[
-  household.power.consumption$Date=='1/2/2007' | 
-  household.power.consumption$Date=='2/2/2007',
-]
+  setwd('~/leNkusWoaceN/ExData_Plotting1')
 
-head(power.consumption)
+  clean.file = 'power_consumption.txt'
+
+  if (file.exists(clean.file)) {
+    power.consumption<-read.table('power_consumption.txt',header=TRUE,sep=';')
+    power.consumption$DateTime <- parse_date_time(power.consumption$DateTime, orders="ymd hms")
+  }
+  else 
+  {  
+    url <- 
+      'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+    file <- 'household_power_consumption.zip'
+    uncompress.file <- 'household_power_consumption.txt'
+    
+    # get file is doesn't already exist
+    if (!file.exists(uncompress.file)) {
+        if (!file.exists(file)) {
+            download.file(url,file,method='curl')
+            dateDownloaded <- date()
+            dateDownloaded
+        }
+        # unzip .zip file
+        unzip(file)
+    }
+    
+    household.power.consumption <- read.table(
+      uncompress.file,
+      header=TRUE,
+      na.strings='?',
+      sep=';',
+      colClasses=fileColClasses(uncompress.file)
+    )
+    
+    work.power.consumption <- household.power.consumption[
+      household.power.consumption$Date=='1/2/2007' | 
+      household.power.consumption$Date=='2/2/2007',
+    ]
+    
+    work.power.consumption$DateTime <- parse_date_time(
+      x = paste(as.Date(work.power.consumption$Date, format = "%m/%d/%Y"), work.power.consumption$Time),
+      orders="ymd hms")
+   
+    power.consumption <- work.power.consumption[,c(10,3:9)]
+    write.table(power.consumption, clean.file, row.name=FALSE, sep=';')
+  }
+  return(power.consumption)
+}

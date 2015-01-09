023d99fa92c7238b713c76f534e8832b4479b203
Mark Anderson <leNkusWoaceN.github.com@ma7.org>
2015-01-08 22:36:53 -0600

    rough draft, need to fix 2,1, replace 1,2  2,2

--- plot4.R
+++ plot4.R
@@ -12,8 +12,52 @@ power.consumption <- getData()
 # open device
 png(filename='plot4.png',width=480,height=480,units='px')
 
-#plot data
-# ???
+par(mfrow=c(2,2))
+
+# 1,1
+plot(
+  power.consumption$DateTime,
+  power.consumption$Global_active_power,
+  type='l',
+  xlab='',
+  ylab='Global Active Power (kilowatts)'
+)
+
+# 1,2
+plot(
+  power.consumption$DateTime,
+  power.consumption$Global_active_power,
+  type='l',
+  xlab='',
+  ylab='Global Active Power (kilowatts)'
+)
+
+# 2,1
+plot(
+  power.consumption$DateTime,
+  power.consumption$Sub_metering_1,
+  type='l',
+  col='black',
+  xlab='',
+  ylab='Energy sub metering'
+)
+lines(power.consumption$DateTime,power.consumption$Sub_metering_2,col='red')
+lines(power.consumption$DateTime,power.consumption$Sub_metering_3,col='blue')
+
+legend(
+  'topright',
+  legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),
+  col   =c('black',         'red',           'blue')
+)
+
+# 2,2
+plot(
+  power.consumption$DateTime,
+  power.consumption$Global_active_power,
+  type='l',
+  xlab='',
+  ylab='Global Active Power (kilowatts)'
+)
 
 # close device
 dev.off()

26789964d607f6113131f1c8e4f82166bc003026
Mark Anderson <leNkusWoaceN.github.com@ma7.org>
2015-01-08 22:27:09 -0600

    add legend, not quite right

--- plot3.R
+++ plot3.R
@@ -24,6 +24,12 @@ plot(
 lines(power.consumption$DateTime,power.consumption$Sub_metering_2,col='red')
 lines(power.consumption$DateTime,power.consumption$Sub_metering_3,col='blue')
 
+legend(
+  'topright',
+  legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),
+  col   =c('black',         'red',           'blue')
+) 
+  
 # close device
 dev.off()
 
